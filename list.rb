d4d7f418f5e067c7d8550deaad242beb6221a859
jcdcm <jcdcm20@gmail.com>
2024-02-19 08:49:54 -0300

    Define append method

--- list.rb
+++ list.rb
@@ -8,5 +8,19 @@ class Node
 end
 
 class LinkedList
-  
+
+  def initialize
+    @head = nil
+    @tail = nil
+  end 
+
+  def append(value)
+    if  @head == nil
+      @head = value
+      @tail = value
+    else
+      value.next = @head
+      @head = value
+    end 
+  end
 end
