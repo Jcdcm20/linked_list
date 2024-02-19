94b2d01f185ee491b9c9bad8c778af4e425340bf
jcdcm <jcdcm20@gmail.com>
2024-02-19 08:54:53 -0300

    Fix append and prepend methods and add size, head, tail methods

--- list.rb
+++ list.rb
@@ -12,20 +12,40 @@ class LinkedList
   def initialize
     @head = nil
     @tail = nil
+    @size = 0
   end 
 
   def append(value)
-    @tail = value
-    value.next = nil
+    if @head.nil?
+      @head = value
+      @tail = value
+    else
+      @tail.next = value
+      @tail = value
+    end
   end
 
   def prepend(value)
-    if  @head == nil
+    if  @head.nil?
       @head = value
       @tail = value
     else
       value.next = @head
       @head = value
     end 
+    @nodes += 1
+  end
+
+  def size
+    @nodes
+  end
+
+  def head
+    @head
+  end
+
+  def tail
+    @tail
   end
 end
+
